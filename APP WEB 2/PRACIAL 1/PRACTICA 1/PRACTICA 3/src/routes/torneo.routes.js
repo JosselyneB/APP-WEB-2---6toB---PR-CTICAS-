9036a10d14b147b538197b40c574101a6a44ace8
JosselyneB <118408836+JosselyneB@users.noreply.github.com>
2024-05-21 22:34:26 -0500

    Update torneo.routes.js

--- APP WEB 2/PRACIAL 1/PRACTICA 1/PRACTICA 3/src/routes/torneo.routes.js
+++ APP WEB 2/PRACIAL 1/PRACTICA 1/PRACTICA 3/src/routes/torneo.routes.js
@@ -1,9 +1,69 @@
-import { Router, Router } from "express";
 
-const Router = Router();
+import { Router } from "express";
+import { prisma } from "../db.js";
 
-router.get('/torneo', (req, res)=>{
-    res.send('torneo')
-})
+const router = Router();
 
-export default Router;
+router.get("/torneo", async (req, res) => {
+	try {
+		const torneo = await prisma.torneo.findMany({
+			include: {
+				category: true,
+			},
+		});
+		res.json(torneo);
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.post("/torneo", async (req, res) => {
+	try {
+		const torneo = await prisma.torneo.create({
+			data: req.body,
+		});
+		res.json(torneo);
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.get("/torneo/:id", async (req, res) => {
+	const torneo = await prisma.torneo.findUnique({
+		where: {
+			id: Number(req.params.id),
+		},
+		include: {
+			category: true,
+		},
+	});
+	res.json(torneo);
+});
+
+router.delete("/torneo/:id", async (req, res) => {
+	const torneo = await prisma.torneo.delete({
+		where: {
+			id: Number(req.params.id),
+		},
+	});
+	res.json(torneo.quantity);
+});
+
+router.patch("/torneo/:id", async (req, res) => {
+	try {
+		const torneo = await prisma.torneo.update({
+			where: {
+				id: Number(req.params.id),
+			},
+			data: req.body,
+			include: {
+				category: true,
+			},
+		});
+		res.json(torneo);
+	} catch (error) {
+		next(error);
+	}
+});
+
+export default router;
